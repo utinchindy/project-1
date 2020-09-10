13891ada4790af55e18234586e4d7f885e25dcb0
utinchindy <utincindyselvira16@gmail.com>
2020-09-10 14:37:23 +0700

    Membuat class kucing

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+
+/**
+ * 
+ */
+class Hewan
+{
+	public function bersuara(){
+		return "Hewan Ini Bersuara : ";
+	} 
+}
+
+/**
+ * 
+ */
+class Kucing extends Hewan
+{
+	
+	public function bersuara()
+	{
+		echo "Meoww";
+	}
+
+	public function jumlah_kaki(){
+		return "Hewan ini memiliki jumlah kaki : ";
+	}
+	public function bisa_terbang(){
+		return "Hewan ini tidak bisa terbang";
+	}
+}
+
+
